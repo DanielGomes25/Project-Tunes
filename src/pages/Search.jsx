7241569540ab00f0906ada4b155391ae9b79ab6e
Daniel <daniel.gomes.9@hotmail.com>
2023-03-14 15:19:09 -0300

    resolve requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import Header from '../components/Header';
 
 class Search extends Component {
+  state = {
+    songsOrBands: '',
+  };
+
+  inputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { songsOrBands } = this.state;
+    const MIN_VALUE = 2;
+    const ableBtn = songsOrBands.length >= MIN_VALUE;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          placeholder="pesquise por banda ou artista"
+          data-testid="search-artist-input"
+          type="text"
+          name="songsOrBands"
+          id="songsOrBands"
+          value={ songsOrBands }
+          onChange={ this.inputChange }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          name="btn"
+          id="btn"
+          disabled={ !ableBtn }
+        >
+          Pesquisar
+
+        </button>
       </div>
     );
   }
