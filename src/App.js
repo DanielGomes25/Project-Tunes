601d98c9cfe968f15af62abf7c0292962bbd9bd0
Daniel <daniel.gomes.9@hotmail.com>
2023-03-13 21:33:27 -0300

    resolve requisito 2

--- src/App.js
+++ src/App.js
@@ -11,18 +11,15 @@ import Search from './pages/Search';
 class App extends React.Component {
   render() {
     return (
-      <>
-        <switch>
-          <Route exact path="/" component={ Login } />
-          <Route exact path="/search" component={ Search } />
-          <Route exact path="/album/:id" component={ Album } />
-          <Route exact path="/favorites" component={ Favorites } />
-          <Route exact path="/profile" component={ Profile } />
-          <Route exact path="/profile/edit" component={ ProfileEdit } />
-          <Route exact path="*" component={ NotFound } />
-        </switch>
-        ;
-      </>
+      <switch>
+        <Route exact path="/" component={ Login } />
+        <Route exact path="/search" component={ Search } />
+        <Route exact path="/album/:id" component={ Album } />
+        <Route exact path="/favorites" component={ Favorites } />
+        <Route exact path="/profile" component={ Profile } />
+        <Route exact path="/profile/edit" component={ ProfileEdit } />
+        <Route exact path="*" component={ NotFound } />
+      </switch>
     );
   }
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,63 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import Loading from './Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
+  state = {
+    nameInput: '',
+    isLoading: false,
+    redirect: false,
+  };
+
+  inputChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  };
+
+  submitBtn = async () => {
+    const { nameInput } = this.state;
+    this.setState({ isLoading: true });
+    const result = await createUser({ name: nameInput });
+    // console.log(result);
+    if (result === 'OK') {
+      this.setState({
+        isLoading: false,
+        redirect: true,
+      });
+    }
+  };
+
   render() {
+    const { nameInput, isLoading, redirect } = this.state;
+    const MAX_VALUE = 3;
+    const ableBtn = nameInput.length >= MAX_VALUE;
+    if (isLoading) return <Loading />;
+    if (redirect) return <Redirect to="/search" />;
     return (
-      <div data-testid="page-login" />
+      <div data-testid="page-login">
+        <input
+          placeholder="digite seu nome"
+          name="nameInput"
+          type="text"
+          data-testid="login-name-input"
+          id="nameInput"
+          value={ nameInput }
+          onChange={ this.inputChange }
+
+        />
+        <button
+          type="button"
+          data-testid="login-submit-button"
+          onClick={ this.submitBtn }
+          disabled={ !ableBtn }
+        >
+          Entrar
+
+        </button>
+      </div>
     );
   }
 }
